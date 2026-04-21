fe50e7d32a51c076105c9fb8bf9abf24acd68e6a
d33sty <distortion616@gmail.com>
2026-04-21 18:35:12 +0300

    21.04.2026

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException, status
 
 app = FastAPI()
 
 
-# Напишите здесь вашу модель.
-class Note(BaseModel):
-    id: int
-    text: str
-
-
-notes = [
-    Note(id=1, text="Купить хлеб"),
-    Note(id=2, text="Написать отчет"),
-    Note(id=3, text="Позвонить маме"),
-    Note(id=4, text="Сходить в спортзал"),
-    Note(id=5, text="Прочитать книгу"),
-]
-
-
 # Напишите здесь ваше решение.
-@app.delete("/notes/{note_id}", status_code=status.HTTP_200_OK, response_model=Note)
-async def delete_note(note_id: int) -> Note:
-    for i, note in enumerate(notes):
-        if note.id == note_id:
-            return notes.pop(i)
+async def check_auth(token: str):
+    if token == "secret":
+        return True
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
+
+@app.get("/profile", status_code=status.HTTP_200_OK)
+async def get_profile(auth: bool = Depends(check_auth)) -> str:
+    if auth:
+        return "User is authorized"
